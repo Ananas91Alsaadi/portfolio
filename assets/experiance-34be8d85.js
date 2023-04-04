bc6a190219fee62eec73b6dd73f71681dcf6c6d9
Ananas91Alsaadi <anas_al-saadi@hotmail.com>
2023-04-04 14:14:06 +0200

    ndist

---
+++ assets/experiance-34be8d85.js
@@ -0,0 +1,21 @@
+import d from"/portfolio/pic4.jpeg";import c from"/portfolio/pic5.jpg";import{_ as m,o,c as s,a as e,F as u,r as g,t as i}from"./index-e52303bd.js";import"/portfolio/pic7.png";import"/portfolio/pic6.png";const h={data(){return{edu:[{title:`May 2022 - June 2022
+                    Intern as web designer and developer
+Webbay/Sweden`,content:`It was my internship. I worked as a designer for both web and mobile applications. I also worked on a website using WordPress to make it. HTML, CSS, and JS are mainly used to program the website. Moreover, a little bit with Flutter platform and dart language.
+`},{title:`August 2018 - August 2019
+Consultant and administrator
+Kronofogdemyndighet/Jönköping, Sweden
+`,content:`The work involved ongoing case management via the Kronofogdens IT system. Case management included gathering information and processing documentation for decisions, registering maintenance by moving customers to different locations in the IT system, and checking payment deviations. The work also included mail handling, archive management, and conversations with other authorities.
+`},{title:`June 2013 - March 2015
+
+Financial manager
+Alaseel/Damascus, Syria
+`,content:`
+          Daily financial follow-up, balance sheet, budget, certify and check that information has been registered in the computer, follow-up of attendance and calculate workers' salaries, draw advertising posters, and maintain computers.
+
+`},{title:`August 2010 - June 2013
+Cost controller
+Ebla/Damascus, Syria
+`,content:`
+          In my role as a Cost Controller, I was responsible for various tasks such as inventory management, budget and balance sheet development, and data recording in the system. Additionally, I demonstrated leadership skills by leading the accounting department team to achieve the set objectives.
+
+`}]}},methods:{handleOver(){this.$emit("ScrollingTrue",!0),console.log("handleOver")},stopListening(){this.$emit("ScrollingFalse",!1),console.log("stopListening")}}},p={class:"container"},f=e("img",{src:d,alt:""},null,-1),b=e("img",{src:c,alt:""},null,-1),v=e("h2",null,"Jobs and Works",-1),y=e("img",{src:"https://blog.lpcinternational.co.uk/hubfs/What%20Skills%20Does%20A%20Supply%20Chain%20Consultant%20Need%20%26%20Why.png",alt:""},null,-1);function _(w,n,k,S,r,a){return o(),s("main",null,[e("div",p,[f,b,v,y,e("div",{onMouseover:n[0]||(n[0]=(...t)=>a.handleOver&&a.handleOver(...t)),onMouseout:n[1]||(n[1]=(...t)=>a.stopListening&&a.stopListening(...t)),class:"blogs scroller"},[(o(!0),s(u,null,g(r.edu,(t,l)=>(o(),s("div",{class:"blog",key:l},[e("h2",null,i(t.title),1),e("p",null,i(t.content),1)]))),128))],32)])])}const M=m(h,[["render",_]]);export{M as default};
